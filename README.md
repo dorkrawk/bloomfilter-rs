df99e93f833563ddd7ac3e4ebf9733a52f3bc15f
Dave <dave.schwantes@gmail.com>
2015-04-13 23:21:05 -0700

    It kinda works now

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # BloomFilter RS
 A Bloom filter written in Rust.
+
+## Example
+```
+let mut bf = BloomFilter::new(10, 1);
+
+bf.insert("hamster");
+bf.insert("coffee");
+
+bf.check("hamster");
+// true
+
+bf.check("oatmeal");
+// (probably) false
+```
+
+# TODO
+- [ ] multiple hash functions
+- [ ] error_chance() function
+
+*note: I mainly built this project to learn a bit about Rust*
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,29 @@
-use std::hash::{hash, Hash, SipHasher};
+use std::hash::{Hash, Hasher, SipHasher};
 
 #[test]
-fn hello() {
-  println!("Hello, bloomfilter-rs!");
+fn create_correct_size() {
+  let bf = BloomFilter::new(10, 1);
+  assert!(bf.buckets.len() == 10);
 }
 
 #[test]
-fn filter_size() {
-  let filter = BloomFilter::new(10, 2);
+fn insert_and_check() {
+  let mut bf = BloomFilter::new(10, 1);
+  bf.insert("dave");
+  bf.insert("hamster");
+  bf.insert("coffee");
+
+  assert!(bf.check("dave") == true);
+  assert!(bf.check("hamster") == true);
+  assert!(bf.check("coffee") == true);
+}
+
+fn my_hash<T>(obj: T) -> u64
+    where T: Hash
+{
+    let mut hasher = SipHasher::new();
+    obj.hash(&mut hasher);
+    hasher.finish()
 }
 
 pub struct BloomFilter {
@@ -18,8 +34,29 @@ pub struct BloomFilter {
 impl BloomFilter {
 
   pub fn new(size: usize, hashes: u32) -> BloomFilter {
-    let mut buckets = vec![false; size];
+    let buckets = vec![false; size];
 
     BloomFilter { buckets: buckets, hashes: hashes }
+  }
+
+  pub fn insert(&mut self, word: &str) {
+    let i:usize = self.bloom_hash(word);
+
+    self.buckets[i] = true;
+  }
+
+  pub fn check(&mut self, word: &str) -> bool {
+    let i:usize = self.bloom_hash(word);
+
+    if self.buckets[i] {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  fn bloom_hash(&mut self, word: &str) -> usize {
+    let the_hash:usize = my_hash(&word) as usize;
+    return the_hash % self.buckets.len();
   }
 }
